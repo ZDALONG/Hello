b7e21c3d2ea1094dc2c86717c293ec9b0f317cfa
ZDALONG <835138980@qq.com>
2019-04-27 20:39:58 +0800

    屏幕旋转+保留旋转之前的数据

--- app/src/main/java/cn/edu/swufe/cheng/hello/SecondActivity.java
+++ app/src/main/java/cn/edu/swufe/cheng/hello/SecondActivity.java
@@ -7,9 +7,10 @@ import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity{
-
+    private final String TAG = "second";
     TextView score;
     TextView score2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,32 @@ public class SecondActivity extends AppCompatActivity{
 
         score = (TextView) findViewById(R.id.score);
         score2 = (TextView)findViewById(R.id.score2);
+        Log.i(TAG,"onCreate: ");
+    }
+
+
+    //调用outState方法和 savedInstanceState方法，保留旋转之前的状态
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String scorea = ((TextView)findViewById(R.id.score)).getText().toString();
+        String scoreb = ((TextView)findViewById(R.id.score2)).getText().toString();
+
+        Log.i(TAG,"onSaveInstanceState: ");
+        outState.putString("teama_score",scorea);
+        outState.putString("teamb_score",scoreb);
+    }
+
+    @Override
+    // savedInstanceState方法进行数据保存，RestoreInstanceState恢复数据
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String scorea = savedInstanceState.getString("teama_score");
+        String scoreb = savedInstanceState.getString("teamb_score");
+
+        Log.i(TAG,"onRestoreInstanceState: ");
+        ((TextView)findViewById(R.id.score)).setText(scorea);
+        ((TextView)findViewById(R.id.score2)).setText(scoreb);
     }
 
     public void btnAdd1(View btn){
